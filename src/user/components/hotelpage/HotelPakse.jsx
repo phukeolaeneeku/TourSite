0df7425208bc26af51f165bf15a4de0fcb3eedb1
phukeolaeneeku <144010043+phukeolaeneeku@users.noreply.github.com>
2024-04-23 17:38:15 +0700

    Update

--- src/user/components/hotelpage/HotelPakse.jsx
+++ src/user/components/hotelpage/HotelPakse.jsx
@@ -6,8 +6,6 @@ import Header from "../header/Header";
 import Menu from "../header/Menu"
 import "./css/hotel.css";
 import { SiGooglemaps } from "react-icons/si";
-import hotel1 from "../../../img/hotel1.jpg";
-import hotel2 from "../../../img/hotel2.jpg";
 import hotel3 from "../../../img/hotel3.jpg";
 import { IoStar } from "react-icons/io5";
 
@@ -17,19 +15,19 @@ function HotelPakse() {
     <>
         <Header />
         <Menu />
-        <div className="containnerHotel_body">
+        <div className="containnerHotel_bodys">
             <div className="content_item_Hotel">
                 <div className="container_txt_head">
                     <h3 className="txt_head_Hotel">
                         <span className="span_Styles"></span>Pakse
                     </h3>
                 </div>
-                <div className="box_container_hotel">
-                    <div className="box_container_body">
+                <div className="box_container_hotels">
+                    <div className="box_container_body_hotel">
                         <div className="container_image">
                             <img src={hotel3} alt="image" />
                         </div>
-                        <div className="container_des">
+                        <div className="container_desc">
                             <h2>Silver Naga Hotel</h2>
                             <p>
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, modi!
@@ -51,37 +49,11 @@ function HotelPakse() {
                         </div>
                     </div>
                     <div className="box_container_body_hotel">
-                        <Link to="/details" className="container_image">
-                            <img src={hotel1} alt="image" />
-                        </Link>
-                        <div className="container_desc">
-                            <h2>Amart Hotel Vang Vieng</h2>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, modi!
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, modi!
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, modi!
-                            </p>
-                            <div className="txt_hotel">
-                                <p className="price_number_hotel">$10</p>
-                                <p className="txt_price"> ￦15,000 </p>
-                            </div>
-                            <p className="SiGooglemaps"><SiGooglemaps id="icon_map"/> Laos</p>
-                            <p className="box_icon_star"> 
-                                <IoStar id="icon_star"/> 
-                                <IoStar id="icon_star"/> 
-                                <IoStar id="icon_star"/> 
-                                <IoStar id="icon_star"/> 
-                                <IoStar id="icon_star"/> 
-                            </p>
-                            
-                        </div>
-                    </div>
-                    <div className="box_container_body">
                         <div className="container_image">
-                            <img src={hotel2} alt="image" />
+                            <img src={hotel3} alt="image" />
                         </div>
-                        <div className="container_des">
-                            <h2>Indigo House Hotel</h2>
+                        <div className="container_desc">
+                            <h2>Silver Naga Hotel</h2>
                             <p>
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, modi!
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, modi!
@@ -91,7 +63,7 @@ function HotelPakse() {
                                 <p className="price_number_hotel">$10</p>
                                 <p className="txt_price"> ￦15,000 </p>
                             </div>
-                            <p className="SiGooglemaps"><SiGooglemaps id="icon_map"/> Luang Prabang</p>
+                            <p className="SiGooglemaps"><SiGooglemaps id="icon_map"/> Vang Vieng</p>
                             <p className="box_icon_star"> 
                                 <IoStar id="icon_star"/> 
                                 <IoStar id="icon_star"/> 
